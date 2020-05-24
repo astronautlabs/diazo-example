3983d499a4c71ca73bd7b0ed62fae0b983427981
William Lahti <wilahti@gmail.com>
2020-05-24 13:18:55 -0700

    simple example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Firegraph, FiregraphNodeSet, FiregraphContext } from '@astronautlabs/firegraph';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,118 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firegraph-example';
+  constructor(
+    private matSnackbar : MatSnackBar
+  ) {
+
+  }
+  readonly = false;
+  active = false;
+  graphContext : FiregraphContext;
+  dirty = false;
+
+  onSaveRequested() {
+    if (this.dirty) {
+      this.dirty = false;
+      this.matSnackbar.open(
+        "Pretending to save your changes!", 
+        undefined, {
+          duration: 1000
+        }
+      );
+    } else {
+      this.matSnackbar.open(
+        "No changes need to be saved!", 
+        undefined, 
+        {
+          duration: 1000
+        }
+      );
+    }
+  }
+
+  onContextChanged(context : FiregraphContext) {
+    this.graphContext = context;
+
+    console.log(`Received graph context from <fg-editor>:`);
+    console.dir(context);
+  }
+
+  onGraphChanged(graph : Firegraph) {
+    this.dirty = true;
+    this.matSnackbar.open('You have changed the graph!', undefined, {
+      duration: 1000
+    });
+  }
+
+  myGraph : Firegraph = {
+    edges: [], 
+    nodes: [
+      {
+        id: 'one',
+        data: { 
+          unit: 'my-input',
+          textProperty: 'hello'
+        },
+        label: 'My Input',
+        x: 50, 
+        y: 50,
+        slots: [
+          { id: 'output', type: 'output', label: 'Output' }
+        ]
+      },
+      {
+        id: 'two',
+        data: { unit: 'my-output' },
+        label: 'My Output',
+        x: 300, 
+        y: 100,
+        slots: [
+          { id: 'input', type: 'input', label: 'Input' }
+        ]
+      }
+    ]
+  };
+  
+  availableNodes : FiregraphNodeSet[] = [
+    {
+        id: 'general',
+        label: 'General',
+        nodes: [
+            {
+                data: {
+                    unit: 'my-input',
+                    someProperty: 'abc'
+                },
+                label: 'My Input',
+                slots: [
+                    { id: 'output', type: 'output', label: 'Output' }
+                ],
+                properties: [
+                    {
+                        id: 'output-options',
+                        label: 'Output Options',
+                        properties: [
+                            {
+                                type: 'text',
+                                path: '$.data.textProperty',
+                                label: 'A Text Property',
+                                description: 'This is an example of a text property!'
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                data: {
+                    unit: 'my-output'
+                },
+                label: 'My Output',
+                slots: [
+                    { id: 'input', type: 'input', label: 'Input' }
+                ]
+            }
+        ]
+    }
+]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FiregraphModule } from '@astronautlabs/firegraph';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    FiregraphModule,
+    MatCheckboxModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
